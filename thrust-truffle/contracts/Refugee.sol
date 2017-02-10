72f82fb6b5b8a2bae90c32c6dc390c4e5135773e
Dragos Laurentiu Barosan <dragos.barosan@ing.nl>
2017-02-11 00:40:20 +0100

    create all setters for smart contract

--- thrust-truffle/contracts/Refugee.sol
+++ thrust-truffle/contracts/Refugee.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.0;
 contract Refugee {
 
-	
+
 	enum Status {
         Rejected,
         Created,
@@ -9,9 +9,8 @@ contract Refugee {
         Approved
     }
 
-    Status status;
-    string dateCreated;
-	
+
+
 	struct Personal {
         string firstName;
 		string lastName;
@@ -32,23 +31,23 @@ contract Refugee {
 		Status status;
 		string dateCreated;
 		}
-		
-		
+
+
 	struct Municipality {
         string name;
 		string country;
 		Status status;
 		string dateCreated;
 		}
-		
-		
+
+
 	struct Rating {
         string rating;
 		string rater;
 		string details;
 		string dateCreated;
 		}
-		
+
 	struct Camp {
         string name;
 		string country;
@@ -57,7 +56,10 @@ contract Refugee {
 		string bed;
 		string dateCreated;
 		}
-	
+
+
+    Status status;
+    string dateCreated;
 	Country country;
 	Municipality municipality;
 	Rating rating;
@@ -75,7 +77,7 @@ contract Refugee {
 		string gender,
 		string bloodGroup,
 		string birthDate);
-		
+
     event RegisterRefugeePersonalData2(address indexed contractAddress,
         string countryOrigin,
         string civilStatus,
@@ -83,13 +85,36 @@ contract Refugee {
 		string documentId,
 		string fingerprintHash,
 		string photoHash);
-		
+
 	event RegisterRefugeeRating(address indexed contractAddress,
         string rating,
         string rater,
 		string details,
 		string dateCreated);
-		
+
+
+    event RegisterRefugeeCountry(address indexed contractAddress,
+        string name,
+        Status status,
+        string dateCreated);
+
+
+    event RegisterRefugeeMunicipality(address indexed contractAddress,
+        string name,
+        string country,
+        Status status,
+        string dateCreated);
+
+
+
+    event RegisterRefugeeCamp(address indexed contractAddress,
+        string name,
+        string country,
+        string municipality,
+        string tent,
+        string bed,
+        string dateCreated);
+
     function setPersonalData( string _firstName,
 		string _lastName,
 		string _city,
@@ -103,7 +128,7 @@ contract Refugee {
 		string _fingerprintHash,
 		string _photoHash,
 		string _dateCreated,
-		string _details) 
+		string _details)
 	{
 		personalData.firstName=_firstName;
 		personalData.lastName=_lastName;
@@ -120,31 +145,57 @@ contract Refugee {
 		dateCreated=_dateCreated;
 
 		status=Status.Created;
-        
+
         LogAction(this,msg.sender, "Created",_details,_dateCreated);
 		RegisterRefugeePersonalData1(this,personalData.firstName,personalData.lastName,personalData.city,personalData.gender,personalData.bloodGroup,personalData.birthDate);
 		RegisterRefugeePersonalData2(this,personalData.countryOrigin,personalData.civilStatus,personalData.documentType,personalData.documentId,personalData.fingerprintHash,personalData.photoHash);
-	
+
 	}
 
 
 	 function setRatingData( string _rating,
 		string _rater,
-		string _details,
-		string _dateCreated) 
+		string details,
+		string _dateCreated,
+		string _details)
 	{
 	 	rating.rating=_rating;
 	 	rating.rater=_rater;
-	 	rating.details=_details;
+	 	rating.details=details;
 	 	rating.dateCreated=_dateCreated;
-	 	
+
   	 	LogAction(this,msg.sender, "Rating",_details,_dateCreated);
   	 	RegisterRefugeeRating(this,_rating,_rater,_details,_dateCreated);
-  	 
+
 	}
-	
 
+	function setCountry (string _name, Status _status , string _dateCreated, string _details) {
 
+      country.name = _name;
+      country.status = _status;
+      country.dateCreated = _dateCreated;
+      LogAction(this,msg.sender, "Country",_details,_dateCreated);
+      RegisterRefugeeCountry(this,country.name, country.status, country.dateCreated);
+    }
+
+    function setMunicipality (string _name, string _country, Status _status, string _dateCreated, string _details) {
+      municipality.name = _name;
+      municipality.country = _country;
+      municipality.status = _status;
+      municipality.dateCreated = _dateCreated;
+      LogAction(this,msg.sender, "Municipality",_details,_dateCreated);
+      RegisterRefugeeMunicipality(this,municipality.name, municipality.country,municipality.status, municipality.dateCreated);
+    }
+
+    function setCamp (string _name, string _country, string _municipality, string _tent, string _bed, string _dateCreated, string _details) {
+      camp.name = _name;
+      camp.country = _country;
+      camp.municipality = _municipality;
+      camp.tent = _tent;
+      camp.bed = _bed;
+      LogAction(this,msg.sender, "Camp",_details,_dateCreated);
+      RegisterRefugeeCamp(this,camp.name, camp.country, camp.municipality,camp.tent, camp.bed, camp.dateCreated);
+    }
 
 
 }
