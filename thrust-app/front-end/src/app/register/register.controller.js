5884eb4075b62740b0260a70a5511b703d4c21e0
Peter Rutgers <p.rutgers@gmail.com>
2017-02-11 14:19:16 +0100

    Put real fields in registration form

--- thrust-app/front-end/src/app/register/register.controller.js
+++ thrust-app/front-end/src/app/register/register.controller.js
@@ -16,15 +16,26 @@
       template: '<md-checkbox ng-model="model[options.key]">{{to.label}}</md-checkbox>'
     });
     
+	formlyConfig.setType({
+		name: 'datepicker',
+		template: '<md-datepicker ng-model="model[options.key]" md-theme="{{to.theme}}"></md-datepicker>'
+	});
+    
+    formlyConfig.setType({
+ 		name: 'select',
+//		template: '<md-select ng-model="model[options.key]" md-theme="{{to.theme}}"><md-option ng-repeat="option in to.options" ng-value="option[to.valueProp || \'value\']">        {{ option[to.labelProp || \'name\'] }}    </md-option></md-select>'
+		template: '<md-select ng-model="model[options.key]" md-theme="{{to.theme}}"><md-option ng-repeat="option in to.options" ng-value="option">        {{ option }}    </md-option></md-select>'
+	});	
+    
     formlyConfig.setWrapper({
       name: 'mdLabel',
-      types: ['input'],
+      types: ['input', 'datepicker', 'select'],
       template: '<label>{{to.label}}</label><formly-transclude></formly-transclude>'
     });
     
     formlyConfig.setWrapper({
       name: 'mdInputContainer',
-      types: ['input'],
+      types: ['input', 'datepicker', 'select' ],
       template: '<md-input-container><formly-transclude></formly-transclude></md-input-container>'
     });
     
@@ -37,9 +48,70 @@
       return '<md-icon class="step" md-font-icon="icon-' + options.data.icon + '"></md-icon>' + template;
     });
   });
+
+  function getCountries() {
+	  
+	  }
+  
+  function getBloodTypes() {
+	  return [
+                    {
+                        "name": "Alberta",
+                        "value":"alberta"
+                    },
+                    {
+                        "name":"British Columbia",
+                        "value":"british_columbia"
+                    },
+                    {
+                        "name":"Manitoba",
+                        "value":"manitoba"
+                    },
+                    {
+                        "name":"New Brunswick",
+                        "value":"new_brunswick"
+                    },
+                    {
+                        "name":"Newfoundland and Labrador",
+                        "value":"newfoundland_and_labrador"
+                    },
+                    {
+                        "name":"Northwest Territories",
+                        "value":"northwest_territories"
+                    },
+                    {
+                        "name":"Nova Scotia",
+                        "value":"nova_scotia"
+                    },
+                    {
+                        "name":"Nunavut",
+                        "value":"nunavut"
+                    },              
+                    {
+                        "name":"Ontario",
+                        "value":"ontario"
+                    },
+                    {
+                        "name":"Prince Edward Island",
+                        "value":"prince_edward_island"
+                    },
+                    {
+                        "name":"Quebec",
+                        "value":"quebec"
+                    },
+                    {
+                        "name":"Saskatchewan",
+                        "value":"saskatchewan"
+                    },
+                    {
+                        "name":"Yukon",
+                        "value":"Yukon"
+                    },
+                ]
+  }
   
 	/** @ngInject */
-	function RegisterController($state, $cookies, logger, formlyVersion) {
+	function RegisterController($state, $cookies, $http, logger, formlyVersion) {
     var vm = this;
     // function assignment
     vm.submitRefugee = submitRefugee;
@@ -60,17 +132,11 @@
     
     vm.fields = [
       {
-        key: 'text',
-        type: 'input',
-        templateOptions: {
-          label: 'Input'
-        }
-      },
-      {
         elementAttributes: {
           layout: 'row',
           'layout-sm': 'column'
         },
+        
         fieldGroup: [
           {
             key: 'firstName',
@@ -91,20 +157,127 @@
         ]
       },
       {
-        key: 'knowsMuffinMan',
-        type: 'checkbox',
+        elementAttributes: {
+          layout: 'row',
+          'layout-sm': 'column'
+        },
+        
+        fieldGroup: [
+          {
+            key: 'documentId',
+            className: 'flex',
+            type: 'input',
+            templateOptions: {
+              label: 'Document ID'
+            }
+          },
+          {
+            key: 'documentType',
+            className: 'flex',
+            type: 'select',
+            templateOptions: {
+              label: 'Document Type',
+              options: ["None", "Passport", "ID card"] 
+            }
+          }
+        ]
+      },
+      {
+        key: 'birthDate',
+        type: 'datepicker',
         templateOptions: {
-          label: 'Do you know the muffin man?'
+          label: 'Date of birth'
+        }
+      },
+      {
+        key: 'gender',
+        type: 'input',
+        templateOptions: {
+          label: 'Gender'
+        }
+      },
+	  {
+        elementAttributes: {
+          layout: 'row',
+          'layout-sm': 'column'
+        },
+        
+        fieldGroup: [
+          {
+            key: 'originCountry',
+            className: 'flex',
+            type: 'select',
+            templateOptions: {
+              label: 'Country of origin',
+              options: ["Syria", "Afghanistan", "Somalia", "Sudan", "Congo"]
+            }
+          },
+          {
+            key: 'registrationCountry',
+            className: 'flex',
+            type: 'select',
+            templateOptions: {
+              label: 'Country of registration',
+              options: ["Belgium", "France", "Germany","Greece", "Italy", "Netherlands", "Spain"]
+            }
+          }
+        ]
+      },
+      {
+        elementAttributes: {
+          layout: 'row',
+          'layout-sm': 'column'
+        },
+        
+        fieldGroup: [
+          {
+            key: 'fingerprintHash',
+            className: 'flex',
+            type: 'input',
+            templateOptions: {
+              label: 'Fingerprint'
+            }
+          },
+          {
+            key: 'photoHash',
+            className: 'flex',
+            type: 'input',
+            templateOptions: {
+              label: 'Photo'
+            }
+          }
+        ]
+      },
+      {
+        key: 'bloodGroup',
+        type: 'select',
+        templateOptions: {
+          label: 'Blood group',
+          options: ['A','B','AB','0']
         }
       }
     ];
+
     
     vm.originalFields = angular.copy(vm.fields);
     
     // function definition
     function submitRefugee() {
       //vm.options.updateInitialValue();
-      alert(JSON.stringify(vm.model), null, 2);
+      var req = {
+		 method: 'POST',
+		 url: '/refugee',
+		 data: JSON.parse(JSON.stringify(vm.model))
+		};
+
+      $http(req).then(success,error);
+    
+      function success(resp) {
+		  console.log(resp.data);
+	  }
+	  function error(resp) {
+		  console.error(resp.data);
+	  }
     }
   };
 
